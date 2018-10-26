75c4d8f7e8b7d4f4a439e6d34a0d9172ced5615f
AbramovYuri <43909379+AbramovYuri@users.noreply.github.com>
2018-10-26 12:12:59 +0300

    Add files via upload

--- parallaxCard.js
+++ parallaxCard.js
@@ -1,20 +1,16 @@
-window.scrollTo(0, 0);
-
 let parallaxCard = document.querySelector('.card');
 let clientWindowHeight = document.documentElement.clientHeight;
 let cardImage = document.querySelectorAll('.card-image');
 let parallaxImage = document.querySelectorAll('.parallax-image');
 let parallaxSection = document.querySelectorAll('.parallax-section');
 let leavingContentDiv = document.querySelectorAll('.leaving-content');
-let leavingContentImage = document.querySelectorAll('.leaving-content-image');
 let yOffset;
 let yOffsetOld = 0;
-let topParallaxImagePosition = -1 * parallaxSection[0].offsetHeight;
 let topImagePosition = -1 * parallaxCard.offsetHeight;
-let topLeavingImagePosition = -1 * leavingContentDiv[0].offsetHeight;
+let topParallaxImagePosition = [];
 
-document.windowOnLoad = parallax();
-
+document.windowOnLoad = resize();
+addEventListener("resize", resize);
 addEventListener("scroll", parallax);
 
 
@@ -32,36 +28,25 @@ function parallaxCardFunc() {
     if (parallaxCard.getBoundingClientRect().top < clientWindowHeight) {
         topImagePosition = topImagePosition - (yOffset - yOffsetOld) / -3;
 
-
-        console.log(parallaxCard.getBoundingClientRect());
-
         for (let i = 0; i < cardImage.length; i++) {
 
             cardImage[i].style.top = topImagePosition + "px";
         }
     }
 }
+
 function parallaxSectionFunc() {
 
     for (let i = 0; i < parallaxSection.length; i++) {
 
         if (parallaxSection[i].getBoundingClientRect().top < clientWindowHeight) {
-            topParallaxImagePosition = topParallaxImagePosition - (yOffset - yOffsetOld)/-2;
-            parallaxImage[i].style.top = topParallaxImagePosition + "px";
+            topParallaxImagePosition[i] = topParallaxImagePosition[i] - (yOffset - yOffsetOld) / -2;
+            parallaxImage[i].style.top = topParallaxImagePosition[i] + "px";
         }
     }
 }
 
 function leavingContent() {
-
-    for (let i =0; i<leavingContentImage.length;i++){
-
-        if (leavingContentImage[i].getBoundingClientRect().top < clientWindowHeight){
-
-            topLeavingImagePosition = topLeavingImagePosition - (yOffset - yOffsetOld)/-2;
-            leavingContentImage[i].style.top = topLeavingImagePosition + "px";
-        }
-    }
 
     if (leavingContentDiv[0].getBoundingClientRect().top < 400) {
         document.querySelectorAll('.leaving-content-left')[0].style.left = "5%";
@@ -71,5 +56,17 @@ function leavingContent() {
     if (leavingContentDiv[0].getBoundingClientRect().top > 400) {
         document.querySelectorAll('.leaving-content-left')[0].style.left = "200%";
         document.querySelectorAll('.leaving-content-right')[0].style.right = "200%";
+    }
+}
+
+function resize() {
+    window.scrollTo(0, 0);
+    yOffsetOld = 0;
+    topImagePosition = -1 * parallaxCard.offsetHeight;
+
+    for (let i = 0; i < parallaxSection.length; i++) {
+        parallaxSection[i].style.height = parallaxSection[i].offsetWidth/2.7 + "px";
+        topParallaxImagePosition[i] = -1 * parallaxSection[i].offsetHeight;
+        parallaxImage[i].style.top = topParallaxImagePosition[i] + "px";
     }
 }
